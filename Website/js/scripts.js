8317b6f0fb82098831ceef3f95f5e4c5d0e9ee8a
dkukreja <powerdester2000@gmail.com>
2016-04-23 03:01:24 -0500

    shopping cart functionality

--- Website/js/scripts.js
+++ Website/js/scripts.js
@@ -39,6 +39,8 @@ function countup(yr,m,d,hr,min){
 }
 
 function toggleCart(){
+    updateCart();
+    
     var opac = $("#darkness").css("opacity");
     if (opac == 0){
         $("#darkness").css("visibility", "visible");
@@ -49,5 +51,65 @@ function toggleCart(){
         $("#darkness").css("opacity", 0);
         $("#cart_sidebar").css("right", '-30%');
         setTimeout('$("#darkness").css("visibility", "hidden");', 500);
+    }
+}
+
+function emptyCart(){
+    localStorage.removeItem("cart");
+    updateCart();
+}
+
+function saveShoe(name, imgSrc, qty){
+    var cartStore = localStorage.getItem("cart");
+    if (cartStore == null){
+        cartStore = "0";
+    }
+    
+    var itemCount = parseInt(cartStore.substr(0,1));
+    itemCount += 1;
+    cartStore = itemCount + cartStore.substr(1,cartStore.length-1);
+    cartStore += name+","+imgSrc+","+qty+"-";
+    localStorage.setItem("cart",cartStore);
+    
+    updateCart();
+}
+
+function updateCart(){
+    var cartStore = localStorage.getItem("cart");
+    if (cartStore == null){
+        cartStore = "0";
+        localStorage.setItem("cart",cartStore);
+        $("#items").html('<div class="button" onclick="emptyCart();">Checkout</div>');
+        console.log("surprise! trap 0");
+    }
+    cartStore = cartStore.substr(1, cartStore.length-1);
+    console.log("trap 1 is\n"+cartStore);
+    
+    var out = "";
+    
+    while (cartStore.indexOf("-") != -1){ //While there's still another new line
+        var line = cartStore.substring(0, cartStore.indexOf("-"));
+        console.log("trap 2 is\n"+line);
+        cartStore = cartStore.slice(cartStore.indexOf("-")+1, cartStore.length);
+        
+        var name = line.slice(0,line.indexOf(","))
+        line = line.substr(line.indexOf(",")+1, line.length-line.indexOf(","));
+        
+        var imageSrc = line.slice(0,line.indexOf(","))
+        line = line.substr(line.indexOf(",")+1, line.length-line.indexOf(","));
+        
+        var quantity = line.slice(0,line.length);
+        line = line.substr(line.indexOf(",")+1, line.length-line.indexOf(","));
+        
+        out += '<div id="shoe">\n<div class="hr"></div>\n<img src="'+imageSrc+'">\n<h1>'+name+'</h1>\n<p>Quantity: '+quantity+'</p>\n</div>';
+        
+        out += '<div class="button" onclick="emptyCart();">Checkout</div>';
+                    
+        $("#items").html(out);
+        
+        console.log("cartStore is\n"+cartStore+"\n");
+        
+        console.log("nextIndex is "+cartStore.indexOf("-"));
+        
     }
 }
